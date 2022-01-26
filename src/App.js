b68c778d7cd86325aabbc0a74da22ac1a7d2d35c
youngG124 <newyoung124@naver.com>
2022-01-26 16:10:34 +0900

    conver pure html to React

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
+
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+            <h1>Web</h1>
+            world wide web!
+      </header>
+    );
+  }
+}
+
+class Navigator extends Component {
+  render() {
+    return (
+      <nav>
+            <ul>
+                <li><a href="1.html"></a>HTML</li>
+                <li><a href="2.CSS"></a>CSS</li>
+                <li><a href="3.JavaScript">JavaScript</a></li>
+            </ul>
+        </nav>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      Hello, React!!!
+      <Subject></Subject>
+      <Navigator></Navigator>
     </div>
     );
   }
