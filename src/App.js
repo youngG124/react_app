952550205ee22cc857e31e88558650502c9824c8
youngG124 <newyoung124@naver.com>
2022-01-26 17:33:41 +0900

    Component 재사용, props

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import './App.css';
 import React, { Component } from 'react';
 
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-            <h1>Web</h1>
-            world wide web!
-      </header>
-    );
-  }
-}
 
-class Navigator extends Component {
+
+class TOC extends Component {
   render() {
     return (
       <nav>
             <ul>
-                <li><a href="1.html"></a>HTML</li>
-                <li><a href="2.CSS"></a>CSS</li>
-                <li><a href="3.JavaScript">JavaScript</a></li>
+                <li><a href="1.html">HTML</a></li>
+                <li><a href="2.html">CSS</a></li>
+                <li><a href="3.html">JavaScript</a></li>
             </ul>
         </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+            <h2>{this.props.title}</h2>
+        {this.props.desc}
+        </article>
+    );
+  }
+}
+
+
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+            <h1>{this.props.title}</h1>
+            {this.props.sub}
+      </header>
     );
   }
 }
@@ -30,8 +44,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Subject></Subject>
-      <Navigator></Navigator>
+      <Subject title="WEB" sub="world wide web!"></Subject>
+      <Subject title="React" sub="For UI"></Subject>
+      <TOC></TOC>
+      <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
     </div>
     );
   }
