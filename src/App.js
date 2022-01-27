daf3813bba68de38fe8828d9dd15a4ba8dc9d2cb
youngG124 <newyoung124@naver.com>
2022-01-27 09:47:32 +0900

    state, key 전체적인 흐름을 다시 공부할 것

--- src/App.js
+++ src/App.js
@@ -10,7 +10,12 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      subject:{title : 'WEB', sub : 'World Wide Web!'}
+      subject:{title : 'WEB', sub : 'World Wide Web!'},
+      contents:[
+        {id:1, title:'HTML', desc:'html is HyperText...'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
     }
   }
 
@@ -23,7 +28,9 @@ class App extends Component {
         </Subject>
       <Subject title="React" sub="For UI"></Subject>
       <MyComponent A="first content, " B="second, " C="third"></MyComponent>
-      <TOC></TOC>
+      <TOC
+        data = { this.state.contents }
+      ></TOC>
       <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
     </div>
     );
