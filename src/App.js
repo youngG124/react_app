afca646c0be5873a4099bf8899cace2ecc5c580d
youngG124 <newyoung124@naver.com>
2022-01-27 09:09:54 +0900

    state

--- src/App.js
+++ src/App.js
@@ -7,10 +7,20 @@ import MyComponent from './components/MyComponent.js'
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject:{title : 'WEB', sub : 'World Wide Web!'}
+    }
+  }
+
   render() {
     return (
       <div className="App">
-      <Subject title="WEB" sub="world wide web!"></Subject>
+      <Subject 
+        title= { this.state.subject.title }
+        sub= { this.state.subject.sub }>
+        </Subject>
       <Subject title="React" sub="For UI"></Subject>
       <MyComponent A="first content, " B="second, " C="third"></MyComponent>
       <TOC></TOC>
